4e93382b94d2a77e09f3b307d24cfe99bf0067bd
Oxshady <shadymahmod55@gmail.com>
2024-10-10 22:06:52 +0300

    Add type annotations to the floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type-annotated function floor which takes a float n as argument"""
+
+
+def floor(n: float) -> int:
+    """Return the floor of the float n"""
+    return int(n)
