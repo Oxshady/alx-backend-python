cd05f14db6eb690678ca483df55272ff7eb4bbac
Oxshady <shadymahmod55@gmail.com>
2024-10-10 22:33:16 +0300

    Add type annotations to the element_length function

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,7 +2,5 @@
 """Type-annotated function element_length that takes a list lst as argument"""
 from typing import List, Tuple, Sequence
 
-
 def element_length(lst: List[Sequence]) -> List[Tuple[Sequence, int]]:
-    """Return a list of tuples where each tuple contains a string and its length"""
     return [(i, len(i)) for i in lst]
