d449df5e63f52c03d32fe3ad9132baa3c8a83f66
Oxshady <shadymahmod55@gmail.com>
2024-10-10 21:59:33 +0300

    Add type annotations to the add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Type-annotated function add that takes
+a float a and a float b as arguments"""
+
+
+def add(a: float, b: float) -> float:
+    """Return the sum of a and b"""
+    return a + b
