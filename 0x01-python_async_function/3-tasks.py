f9083eac9210dc0e0eda2fad2d05fe45677493c4
Oxshady <shadymahmod55@gmail.com>
2024-10-15 00:09:22 +0300

    Add measure_time function to measure and display the total runtime of wait_n; Add task_wait_random function to create and return an asyncio Task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,5 +5,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(n: int) -> asyncio.Task:
-    """return async task"""
+    """
+    function that takes an integer
+    argument and returns a coroutine
+    """
     return asyncio.create_task(wait_random(n))
