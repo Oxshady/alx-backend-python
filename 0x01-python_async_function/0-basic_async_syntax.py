c1f9ed6841b51c37c65c4c48a552cabee480ff6a
Oxshady <shadymahmod55@gmail.com>
2024-10-14 22:08:49 +0300

    implement async wait_random function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,8 +4,8 @@ import asyncio
 from random import uniform
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 1) -> float:
     """asynchronous coroutine that takes in an integer argument"""
-    delay = uniform(0, max_delay)
+    delay: float = uniform(0, max_delay)
     await asyncio.sleep(max_delay)
     return delay
