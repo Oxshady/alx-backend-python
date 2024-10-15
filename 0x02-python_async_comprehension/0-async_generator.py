f9d8e6e2fbb883afbfb1e5f79e592c317f6b0472
Oxshady <shadymahmod55@gmail.com>
2024-10-16 01:07:20 +0300

    improving readability

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,6 +9,6 @@ from typing import AsyncGenerator
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """coroutine will collect 10 random numbers using an async comprehensing"""
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield uniform(0, 10)
