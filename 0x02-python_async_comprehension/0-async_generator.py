9256702351259631e5b986ea2f65cfff2f07d069
Oxshady <shadymahmod55@gmail.com>
2024-10-16 01:01:24 +0300

    Add async_generator coroutine to collect 10 random numbers using async comprehensions

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+The coroutine will collect 10 random numbers using an async comprehensing over
+"""
+import asyncio
+from random import uniform
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """coroutine will collect 10 random numbers using an async comprehensing"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield uniform(0, 10)
