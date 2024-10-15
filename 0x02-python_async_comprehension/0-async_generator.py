afe912ccba68039d622f67323ff4084eb98b25b3
Oxshady <shadymahmod55@gmail.com>
2024-10-16 01:09:11 +0300

    changing return to Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ The coroutine will collect 10 random numbers using an async comprehensing over
 """
 import asyncio
 from random import uniform
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """coroutine will collect 10 random numbers using an async comprehensing"""
     for _ in range(10):
         await asyncio.sleep(1)
